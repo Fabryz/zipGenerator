214c790b295eb59b2e4ace267f068309e0111575
fcodello <fcodello@h-art.it>
2012-11-22 12:27:23 +0100

    testing save to fs

--- zipGenerator.js
+++ zipGenerator.js
@@ -1,27 +1,78 @@
 var spawn = require('child_process').spawn,
+    exec = require('child_process').exec,
+    fs = require('fs'),
+    program = require('commander'),
     gitdiff = null,
     dir = null,
     SHA1 = null,
-    SHA2 = null;
+    SHA2 = null,
+    diffSaveDir = './diffs';
+
+
+    program
+      .version(JSON.parse(fs.readFileSync(__dirname +'/package.json', 'utf8')).version)
+      .option('Posteimpresa Git dir', 'specify the port Posteimpresa git repository directory', String)
+      .option('SHA1', 'specify the first Git SHA Hash', Number)
+      .option('SHA2', 'specify the second Git SHA Hash', Number)
+      .parse(process.argv);
+
+    // program
+    //   .command('dozip [dir]')
+    //   .description('Creates the ZIP with the DIFF file, using the two hashtags')
+    //   .action(function(dir) {
+    //     console.log(dir +' '+ SHA1 +' '+ SHA2);
+    //   });
+
+// check if directory exists
+function dirExistsSync(dir) {
+  try {
+    return fs.statSync(dir).isDirectory();
+  } catch (err) {
+    return false;
+  }
+}
+
+function initDiffDir() {
+  // create diffSaveDir if does not exists
+  if (!dirExistsSync(diffSaveDir)) {
+    fs.mkdir(diffSaveDir, function(err) {
+      if (err) {
+        console.log('initDiffDir ERR:'+ err);
+      }
+    });
+  } else {
+    // if exists remove all directory contents
+    child = exec('rm -Rf '+ diffSaveDir +'/*');
+  }
+}
+
+function saveDiff(diffData) {
+  fs.writeFile(diffSaveDir +'/diff.txt', diffData, 'binary', function(err) {
+    if (err) {
+      console.log('saveDiff ERR:'+ err);
+    }
+  });
+}
 
 function init() {
-    // Array starts from the params
-    var params = process.argv.splice(2);
+  // Array starts from the params
+  var params = process.argv.splice(2);
 
-    dir = params[0];
-    SHA1 = params[1];
-    SHA2 = params[2];
+  dir = params[0];
+  SHA1 = params[1];
+  SHA2 = params[2];
 
-    // console.log('* Starting directory: ' + process.cwd());
-    try {
-      process.chdir(dir);
-      // console.log('* New directory: ' + process.cwd());
-    }
-    catch (err) {
-      console.log('* chdir ERR: ' + err);
-    }
+  // console.log('* Starting directory: ' + process.cwd());
+  try {
+    process.chdir(dir);
+    // console.log('* New directory: ' + process.cwd());
+  }
+  catch (err) {
+    console.log('* chdir ERR: ' + err);
+  }
 
-    gitdiff = spawn('git', ['diff', '--name-only', SHA1, SHA2]);
+  initDiffDir();
+  gitdiff = spawn('git', ['diff', '--name-only', SHA1, SHA2]);
 }
 
 /*
@@ -31,7 +82,9 @@ function init() {
 init();
 
 gitdiff.stdout.on('data', function (data) {
-  console.log('* DIFFS *\n'+ SHA1 +' '+ SHA2 +'\n\n'+ data);
+  diffData = SHA1 +' '+ SHA2 +'\n\n'+ data;
+  console.log('* DIFFS *\n'+ diffData);
+  saveDiff(diffData);
 });
 
 gitdiff.stderr.on('data', function (data) {
